886aff2fcb8f6ca7f5700a338d703998498ddfd5
NikaBotchorishvili <nikabotchorishvili@gmail.com>
2023-10-25 18:16:40 +0400

    wavy thingy

--- front-end/src/layout/client/rootLayout.tsx
+++ front-end/src/layout/client/rootLayout.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 import { ScrollRestoration } from "react-router-dom";
-
+import Sidebar from "../../components/commons/Sidebar";
 const RootLayout: React.FC = () => {
-  return (
-    <>
-      <Outlet />
-      <ScrollRestoration />
-    </>
-  );
+	return (
+		<>
+			<Sidebar />
+			<Outlet />
+			<ScrollRestoration />
+		</>
+	);
 };
 
 export default RootLayout;
