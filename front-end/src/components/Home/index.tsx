7db0d53d2d00c203384e1a324f21d3c88bbee192
Nika Botchorishvili <nikabotchorishvili@gmail.com>
2023-11-11 17:49:48 +0400

    commmit

--- front-end/src/components/Home/index.tsx
+++ front-end/src/components/Home/index.tsx
@@ -6,28 +6,6 @@ import HomeSubscribe from "./components/subscribe/index.";
 import HomeServices from "./components/services";
 
 const Home = () => {
-<<<<<<< Updated upstream
-  return (
-    <>
-      <main className="mt-16 mb-40 text-center">
-        <section className="flex justify-center w-[100dvw]">
-          <Canvas width={400} height={400} />
-        </section>
-        <SwipingText />
-        <HackerTitle text="WE ARE ZAPPY" intervalTime={40}/>
-      </main>
-      {/*About Us */}
-      <HomeAbout />
-      {/* Services */}
-      <HomeServices />
-      {/* Feature Project */}
-      <HomeFeatureProject />
-
-      {/* Subscribe */}
-      <HomeSubscribe />
-    </>
-  );
-=======
 	return (
 		<>
 			<main className="mt-16 mb-40 text-center">
@@ -45,7 +23,6 @@ const Home = () => {
 			<HomeFeatureProject />
 		</>
 	);
->>>>>>> Stashed changes
 };
 
 export default Home;
