34bfc431ef67c3710e906b5e5f8de21021c9634b
Nika Botchorishvili <nikabotchorishvili@gmail.com>
2023-11-12 17:43:26 +0400

    contact us

--- front-end/src/pages/contact/index.tsx
+++ front-end/src/pages/contact/index.tsx
@@ -1,13 +1,15 @@
 import { useForm } from "react-hook-form";
 import Input from "./components/Input";
+import { useTranslation } from "react-i18next";
+
 type FormData = {
-	name: string;
-	lastName: string;
 	email: string;
+	name: string;
+	subject: string;
 	content: string;
 };
-
 const Contact = () => {
+	const [t] = useTranslation();
 	const {
 		register,
 		handleSubmit,
@@ -20,55 +22,72 @@ const Contact = () => {
 	});
 	return (
 		<section className="flex flex-col w-screen items-center my-10 gap-y-12">
-			<h1 className="text-5xl">Contact Us</h1>
+			<h1 className="text-5xl">{t("global.contact.header")}</h1>
 			<form
 				noValidate
 				onSubmit={submitHandler}
 				className="flex flex-col gap-y-8 min-w-[300px]"
 			>
 				<Input
-					label="Enter Your Name"
-					placeholder="John"
+					label={t("global.contact.fields.labels.name")}
+					placeholder={t("global.contact.fields.placeholders.name")}
 					register={register("name", {
-						required: "Name is required",
+						required: t("global.contact.fields.errors.name"),
 					})}
 					error={errors.name}
 				/>
 				<Input
-					label="Enter Your last name"
-					placeholder="Doe"
-					register={register("lastName", { required: "Last name is required" })}
-					error={errors.lastName}
-				/>
-				<Input
-					label="Enter Your Email"
-					placeholder="john@example.com"
+					label={t("global.contact.fields.labels.email")}
+					placeholder={"john@example.com"}
 					register={register("email", {
-						required: "Email is required",
+						required: t(
+							"global.contact.fields.errors.email.required"
+						),
 						pattern: {
 							value: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
-							message: "Invalid Email",
+							message: t(
+								"global.contact.fields.errors.email.validation"
+							),
 						},
 					})}
 					error={errors.email}
+				/>
+
+				<Input
+					label={t("global.contact.fields.labels.subject")}
+					placeholder={t(
+						"global.contact.fields.placeholders.subject"
+					)}
+					register={register("subject", {
+						required: t("global.contact.fields.errors.subject"),
+					})}
+					error={errors.subject}
 				/>
 				<div className="flex flex-col gap-y-2">
 					<div className="relative w-full">
 						<textarea
 							className="p-[10px] w-full h-[200px] border-2 border-t-0 border-b-0 border-[#0B2447] bg-transparent outline-none shadow-[7px_7px_0px_0px_#0B2447] resize-none transition-all duration-500 focus:shadow-none peer focus:placeholder-transparent"
-							{...register("content", { required: "Content can't be empty" })}
-							placeholder="content"
+							{...register("content", {
+								required: t(
+									"global.contact.fields.errors.content"
+								),
+							})}
+							placeholder={t(
+								"global.contact.fields.placeholders.content"
+							)}
 						/>
 						<label
 							className="absolute top-[10px] left-[10px] text-[#0B2447] z-[1] scale-0 peer-focus:scale-100 transition-all peer-focus:-top-[10px] peer-focus:duration-500 peer-focus:transition-all duration-500"
 							htmlFor="input"
 						>
-							Enter Your Content
+							{t("global.contact.fields.labels.content")}
 						</label>
 						<div className="absolute content-[''] bg-[#0B2447] w-[0%] h-[2px] right-0 top-0 peer-focus:w-[35%] peer-focus:transition-all peer-focus:duration-500"></div>
 						<div className="absolute content-[''] bg-[#0B2447] w-[0%] h-[2px] right-0 bottom-0 peer-focus:w-full peer-focus:transition-all peer-focus:duration-500"></div>
 					</div>
-					<small className="text-red-900 text-sm">{errors.content?.message}</small>
+					<small className="text-red-900 text-sm">
+						{errors.content?.message}
+					</small>
 				</div>
 
 				<button className="bg-black text-white py-4 text-xl font-bold rounded-xl">
