906138ab5bb70a7dd9dd34999212f920ed154454
Lomsk1 <lomsianidzegiorgi123@gmail.com>
2023-11-17 17:14:19 +0400

    vercel analitics

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -14,6 +14,7 @@ import Careers from "./pages/careers";
 import Events from "./pages/events";
 import Blogs from "./pages/blogs";
 import { inject } from "@vercel/analytics";
+import { useEffect } from "react";
 
 const clientRoute = createBrowserRouter(
   createRoutesFromElements(
@@ -31,7 +32,9 @@ const clientRoute = createBrowserRouter(
 );
 
 function App() {
-  inject();
+  useEffect(() => {
+    inject();
+  }, []);
   return <RouterProvider router={clientRoute} />;
 }
 
